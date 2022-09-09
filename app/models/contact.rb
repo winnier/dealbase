5c7519ccf0c8c5538ea153b0725a55a4446f4eb5
Will <swilson.carlson@hotmail.com>
2022-09-09 16:38:17 -0400

    added table for contacts page with dynamic headings using serializers and nested attribute data

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,12 @@ class Contact < ApplicationRecord
     has_many :deals, through: :contact_deals
     belongs_to :company
     belongs_to :owner
+
+    def company_info
+        {id: company.id, name: company.name}
+    end
+
+    def owner_info
+        {id: owner.id, name: owner.name}
+    end
 end
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,3 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :phone_number, :address, :linkedin_url, :company, :owner
+  attributes :id, :name, :email, :phone_number, :address, :linkedin_url, :company_info, :owner_info
 end
