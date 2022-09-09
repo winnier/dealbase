e8d5c24e09592d14370694579b621a3d60df9e90
Haala <haalahadithy@gmail.com>
2022-09-09 13:09:19 -0400

    edited the header, footer, and navbar, created style folder. created navbar and header css files. edited the app.css page. Created state for the data that should be rendered in the table page(ex CompanyPage, etc), created a handle click event for to route to the table pages. Also a bit more editing of the routes.

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -2,6 +2,33 @@ const LandingPage = ({handleContactsClick, handleCompaniesClick, handleDealsClic
 
     return (
         <div>
+
+            <div className="landing-summary-div" id="your-summary">
+
+                <div className="contacts-summary-div" onClick={(event)=>handleContactsClick(event, 'yours')}>
+                </div>
+
+                <div className="companies-summary-div" onClick={(event)=>handleCompaniesClick(event, 'yours')}>
+                </div>
+
+                <div className="deals-summary-div" onClick={(event)=>handleDealsClick(event, 'yours')}>
+                </div>
+
+            </div>
+
+            <div className="landing-summary-div" id="company-summary">
+           
+                <div className="contacts-summary-div" onClick={(event)=>handleContactsClick(event, 'all')}>
+                </div>
+
+                <div className="companies-summary-div" onClick={(event)=>handleCompaniesClick(event, 'all')}>
+                </div>
+
+                <div className="deals-summary-div" onClick={(event)=>handleDealsClick(event, 'all')}>
+                </div>
+
+
+            </div>
             
             <div className="landing-summary-div" id="your-summary">
 
@@ -34,11 +61,6 @@ const LandingPage = ({handleContactsClick, handleCompaniesClick, handleDealsClic
     )
 
 
-
-
-    <h3 className="nav-item" >Contacts</h3>
-    <h3 className="nav-item" >Companies</h3>
-    <h3 className="nav-item"  >Deals</h3>
 }
 
 export default LandingPage;
