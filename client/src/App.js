e8bfd0e9bb9e1c27f86d373eb01eecbc21be6d9c
Will <swilson.carlson@hotmail.com>
2022-09-08 20:17:24 -0400

    just starting out

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 // import { BrowserRouter } from "react-router-dom";
 // import { render } from "react-dom";
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [contacts, setContacts] = useState([])
+
+
+  // I'm using this faker gem in meantime until we get data from backend
+
+  const fetchContacts = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const contactsArray = await response.json()
+    setContacts(contactsArray)
+    console.log(contacts)
+  }
+
+
+  useEffect(() => {
+    fetchContacts()
+  }, )
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
