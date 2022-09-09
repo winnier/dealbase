17d1d72078129b66c5d0728c307a8ab9d702f52c
Will <swilson.carlson@hotmail.com>
2022-09-09 15:56:21 -0400

    merge conflict

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,7 @@
 class ContactsController < ApplicationController
     def index
-<<<<<<< HEAD
         contacts = Contact.all
         render json: contacts
-=======
-        render json: Contact.all
->>>>>>> b66e25f (working on crud)
     end
 
     def create
