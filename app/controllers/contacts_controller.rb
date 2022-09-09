94bddd5c98058fed604c40b5f24fbbb818843c46
Aaron Fowlkes <afowlkes21@gmail.com>
2022-09-09 14:48:47 -0400

    working on crud

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,11 @@
 class ContactsController < ApplicationController
     def index
+<<<<<<< HEAD
         contacts = Contact.all
         render json: contacts
+=======
+        render json: Contact.all
+>>>>>>> b66e25f (working on crud)
     end
 
     def create
--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,6 @@ Rails.application.routes.draw do
 
   get '/contacts', to: 'contacts#index'
 
+  get '/contacts', to: 'contacts#index'
+
 end
