8ccbefa0fb6a3809ae8af5768c002c7e7342dafd
Aaron Fowlkes <afowlkes21@gmail.com>
2022-09-09 11:00:05 -0400

    adjusted seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,49 +6,63 @@ Deal.destroy_all
 ContactDeal.destroy_all
 
 puts "Making owners..."
-owner1 =Owner.create!(name: "Aaron", email: "aaron@gmail.com", username: "aaron", password_digest: "aaron")
+owner1 = Owner.create!(name: "Aaron", email: "aaron@gmail.com", username: "aaron", password_digest: "aaron")
 owner2 = Owner.create!(name: "Antonio", email: "antonio@gmail.com", username: "antonio", password_digest: "antonio")
 owner3 = Owner.create!(name: "Will", email: "will@gmail.com", username: "will", password_digest: "will")
 owner4 = Owner.create!(name: "Haala", email: "haala@gmail.com", username: "haala", password_digest: "haala")
 owner5 = Owner.create!(name: "Winnie", email: "winnie@gmail.com", username: "winnie", password_digest: "winnie")
 
-puts "Making contacts..."
-number_of_contacts = 20
-number_of_contacts.times {Contact.create!(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    phone_number: Faker::PhoneNumber.phone_number,
-    address: Faker::Address.full_address,
-    linkedin: "linkedin.com/#{self.name}",
-    company_id: rand(1..20),
-    owner_id: rand(1..5)
-    )}
-
 puts "Creating company..."
-20.times {Company.create!(
-    name: Faker::Company.name,
+20.times {
+    company_name = Faker::Company.name
+    Company.create!(
+    name: company_name,
     address: Faker::Address.full_address,
     country: Faker::Address.country,
     industry: Faker::Job.field,
-    linkedin_url: "linkedin.com/#{self.name}",
-    website: "#{self.name}.com",
-    owner_id: rand(1..5)
+    linkedin_url: "linkedin.com/#{company_name}",
+    # linkedin_url: "linkedin.com/example",
+    # website: "#{self.name}.com",
+    website: "#{company_name}.com",
+    owner_id: Owner.all.sample.id
 )}
 
+puts "Making contacts..."
+20.times {
+    name = Faker::Name.name
+    Contact.create!(
+    # name: Faker::Name.name,
+    name: name,
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.full_address,
+    # linkedin: "linkedin.com/#{self.name}",
+    linkedin: "linkedin.com/#{name}",
+    # linkedin: "linkedin.com/example",
+    # company_id: rand(1..20),
+    # owner_id: rand(1..5)
+    company_id: Company.all.sample.id,
+    owner_id: Owner.all.sample.id 
+    )}
+
 puts "Making deals..."
-20.times {Deal.create!(
-    product: Faker::Commerce.product_name,
-    name: "#{Company.all.sample.name} - #{self.product}",
-    value: rand(100..1000),
+20.times {
+    company = Company.all.sample
+    product = Faker::Commerce.product_name
+    Deal.create!(
+    product: product,
+    name: "#{Company.all.sample.name} - #{product} deal",
+    # name: "#{company.name} - deal example",
+    value: rand(1000..9000),
     active: true,
     status: "",
-    company_id: rand(1..5)
+    company_id: company.id
 )}
 
 puts "Making join table..."
 50.times {ContactDeal.create!(
-    contact_id: rand(1..20),
-    deal_id: rand(1..20),
-    company_id: rand(1..20)
+    contact_id: Contact.all.sample.id,
+    deal_id: Deal.all.sample.id,
+    company_id: Company.all.sample.id
 )}
 
