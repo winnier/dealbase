c934a1c730952283b679a7aacc6e3704d7beb76f
Will <swilson.carlson@hotmail.com>
2022-09-09 15:00:47 -0400

    cors resolved in gem & application.rb files

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ module Dealbase
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
