8facb5eed0b2c90044df613512c0efe729326809
Aaron Fowlkes <afowlkes21@gmail.com>
2022-09-13 11:41:10 -0400

    minor changes

--- client/src/Components/DealItem.js
+++ client/src/Components/DealItem.js
@@ -1,15 +1,49 @@
 const DealItem = ({ name, product, value, stage }) => {
 
+    const numDisplayer = (number) => {
+        if (Math.floor(number) != number) {
+            let x = number.toString()
+            let numArr = x.split('')
+            console.log(numArr)
+            let len = numArr.length
+            let a = numArr.indexOf('.')
+            for (let pos = a - 1; pos > 0; pos--) {
+                if ((a - pos) % 3 == 0 && len - pos != 0) {
+                    numArr.splice(pos, 0, ',')
+                }
+            }
+            let numWithCommas = ""
+            for (let i = 0; i < numArr.length; i++) {
+                numWithCommas += numArr[i]
+            }
+            return numWithCommas
+        } else if (Math.floor(number) == number) {
+            let x = number.toString()
+            console.log(x)
+            let numArr = x.split('')
+            console.log(numArr)
+            let len = numArr.length
+            for (let pos = numArr.length; pos > 0; pos--) {
+                if ((len - pos) % 3 == 0 && len - pos != 0) {
+                    numArr.splice(pos, 0, ',')
+                }
+            }
+            let numWithCommas = ""
+            for (let i = 0; i < numArr.length; i++) {
+                numWithCommas += numArr[i]
+            }
+            numWithCommas = numWithCommas + '.00'
+            return numWithCommas
+        }
+    }
 
     return (
-        <div>
             <tr>
                 <td>{name}</td>
                 <td>{product}</td>
-                <td>{`$${value}`}</td>
+                <td>{`$${numDisplayer(value)}`}</td>
                 <td>{stage}</td>
             </tr>
-        </div>
     )
 }
 
