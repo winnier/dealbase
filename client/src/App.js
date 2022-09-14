5641c779d80847de2271b8906b129f8468f6c602
Will <swilson.carlson@hotmail.com>
2022-09-14 12:58:58 -0400

    committing so we can merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,48 @@
 // import { render } from "react-dom";
 import { Routes, Route, BrowserRouter, useNavigate } from 'react-router-dom';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from 'react-dnd-html5-backend';
+
+
 import CompaniesPage from './Components/CompaniesPage';
 import CompanyCard from './Components/CompanyCard';
+import ContactsPage from './Components/Contact/ContactsPage';
+
 import LandingPage from './Components/LandingPage';
 import DealsPage from './Components/DealsPage';
 import Footer from './Components/Footer';
-import DealCard from './Components/DealCard'
-
-
-import ContactsPage from './Components/Contact/ContactsPage';
 import ContactCard from './Components/Contact/ContactCard';
 import AddNewContact from './Components/Contact/AddNewContact';
 import EditContact from './Components/Contact/EditContact';
+
+
 
 import './App.css';
 import './Styles/Footer.css';
 // import './Styles/Header.css';
 import './Styles/PageStyle.css';
 import NavBar from './Components/NavBar';
-import PipelinePage from './Components/PipelinePage';
+import PipelinePage from './Components/PipelinePage'
 
 function App() {
+
+
+
   return (
       <div>
-
-        <DndProvider backend={HTML5Backend}>
-          <BrowserRouter>
-            <NavBar />
-            <Routes > 
-              <Route path="/" element={ <LandingPage />} />
-              <Route path="/contacts_page" element={<ContactsPage />} />
-              <Route path='/contact_profile/:id' element={<ContactCard />}/>
-              <Route path='/deal_profile/:id' element={<DealCard />}/>
-              <Route path="/companies_page" element={<CompaniesPage />} />
-              <Route path="/companies/:id" element={<CompanyCard />}/>
-              <Route path='/new_contact' element={<AddNewContact />} />
-              <Route path='/edit_contact' element={<EditContact />} />
-              <Route path="/deals_page" element={<DealsPage />} />
-              <Route path="/pipeline_page" element={<PipelinePage />} />
-            </Routes > 
-          </BrowserRouter>
-          <Footer />
-        </DndProvider>
-
+        <BrowserRouter>
+          <NavBar />
+          <Routes > 
+            <Route path="/" element={ <LandingPage />} />
+            <Route path="/contacts_page" element={<ContactsPage />} />
+            <Route path='/contact_profile/:id' element={<ContactCard />}/>
+            <Route path="/companies_page" element={<CompaniesPage />} />
+            <Route path="/companies/:id" element={<CompanyCard />}/>
+            <Route path='/new_contact' element={<AddNewContact />} />
+            <Route path='/edit_contact' element={<EditContact />} />
+            <Route path="/deals_page" element={<DealsPage />} />
+            <Route path='/pipeline_page' element={<PipelinePage />}/>
+          </Routes > 
+        </BrowserRouter>
+        <Footer />
       </div>
 
   );
