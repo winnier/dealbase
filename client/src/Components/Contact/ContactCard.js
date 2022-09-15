7a5042ff895a823ed288a2edf34819ce254f730d
Will <swilson.carlson@hotmail.com>
2022-09-15 15:10:15 -0400

    contact card styling first commit

--- client/src/Components/Contact/ContactCard.js
+++ client/src/Components/Contact/ContactCard.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams, NavLink } from "react-router-dom"; // this lets you destructure the id out of the parameters. 
 import EditContact from "./EditContact";
+import styles from './ContactCard.css'
+import WebFont from 'webfontloader';
 
 const ContactCard = () => {
     let {id} = useParams();
@@ -66,28 +68,65 @@ const ContactCard = () => {
 
     return (
         
-        <div>
-            <button onClick={handleContactDeleteClick}>Delete Contact</button>
-            <button onClick={handleEditClick}>Edit Contact</button>
-            {/* <NavLink className='editContact' to="/edit_contact"><button>Edit Contact</button></NavLink> */}
-            {/* <NavLink className='editContact' to="/edit_contact" state={{ from: "ContactCard"}}><button>Edit Contact</button></NavLink> */}
-            <h4>Name: {contact.name}</h4>
-            <h4>Email: {contact.email}</h4>
-            <h4>Phone: {contact.phone_number}</h4>
-            <h4>Adress: {contact.address}</h4>
-            <h4>Linkedin: {contact.linkedin_url}</h4>
-            <h4>Company: {contact.company_name}</h4>
-            <h4>Owner: {contact.owner_name}</h4>
-            <form className='form' onClick={handleAddNote}>
-            <textarea className='newNoteInput' value={newNote} onChange={(e) => setNewNote(e.target.value)}/>
-            <button className="button">Add Note</button>
-            </form>
-            <h4 className='notesheader'>All Notes</h4>
-            <ul className='notes'>
-                {notes.map((note) => {
-                    return <li className='note' key={note.id}>{`note created: ${note.created_at.substring(0, 10)} note by: ${note.owner_name} note content: ${note.content}`}</li>
-                })}
-            </ul>
+        <div className='contact-card'>
+                <div className="main">
+                <button className="button" onClick={handleContactDeleteClick}>Delete Contact</button>
+                <button className="button" onClick={handleEditClick}>Edit Contact</button>
+                {/* <NavLink className='editContact' to="/edit_contact"><button>Edit Contact</button></NavLink> */}
+                {/* <NavLink className='editContact' to="/edit_contact" state={{ from: "ContactCard"}}><button>Edit Contact</button></NavLink> */}
+                <div className="main">
+                    <div className="topbar">
+
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4 mt-1">
+                        <div className="card text-center sidebar">
+                            <div className="card-body">
+                                <div className="card-text">
+                                    <div className="card-text-head">
+                                        <h3 className="card-data">{contact.name}</h3>
+                                        <h4 className="card-data">{contact.email}</h4>
+                                    </div>
+                                    <hr></hr>
+                                    <div className="card-text-body">
+                                        <h4 className='card-data'>Email: {contact.email}/</h4>
+                                        <h4 className="card-data">Phone: {contact.phone_number}</h4>
+                                        <h4 className="card-data">Adress: {contact.address}</h4>
+                                        <h4 className="card-data">Linkedin: {contact.linkedin_url}</h4>
+                                        <h4 className="card-data">Company: {contact.company_name}</h4>
+                                        <h4 className="card-data">Owner: {contact.owner_name}</h4>
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-8 mt-1">
+                        <div className="card mb-3 content">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-md-3">
+                                        <h4 className='notesheader'>All Notes</h4>
+                                        <ul className='notes'>
+                                            {notes.map((note) => {
+                                                return <li className='note' key={note.id}>{`note created: ${note.created_at.substring(0, 10)} note by: ${note.owner_name} ${note.content}`}</li>
+                                            })}
+                                        </ul>
+                                        <form className='form' >
+                                            <textarea className='newNoteInput' value={newNote} onChange={(e) => setNewNote(e.target.value)}/>
+                                            <button className="button newNoteInput" onClick={handleAddNote}>Add Note</button>
+                                        </form>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
             {isEditClicked?
             <EditContact fetchContact={fetchContact} contact={contact} setContact={setContact}/> : null}
         </div>
