6121bf7f0af9daae7cdfea6d095ffd88b909e9ed
Aaron Fowlkes <afowlkes21@gmail.com>
2022-09-15 14:51:38 -0400

    update working for companies

--- client/src/Components/Company/CompanyCard.js
+++ client/src/Components/Company/CompanyCard.js
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import RenderCompanyDeals from './RenderCompanyDeals'
+import RenderCompanyContacts from './RenderCompanyContacts'
+import EditCompany from './EditCompany'
 
 const CompanyCard = () => {
 
     let {id} = useParams();
     const [company, setCompany] = useState({})
+    let navigate = useNavigate()
+
+
+    const backToCompanies = () => {
+        navigate('/companies_page')
+    }
 
     const fetchCompany = async () => {
         const response = await fetch(`http://localhost:3000/companies/${id}`)
@@ -17,9 +26,78 @@ const CompanyCard = () => {
         fetchCompany()
     }, [])
 
+    const handleCompanyDeleteClick = () => {
+        fetch(`http://localhost:3000/companies/${id}`, {
+            method: "DELETE",
+        })
+            .then(alert('Company has been deleted'))
+        backToCompanies()
+    }
+
+    let [companyDealsArray, setCompanyDealsArray] = useState([])
+
+    const fetchCompanyDeals = async () => {
+        const req = await fetch(`http://localhost:3000/company/${id}/deals`)
+        const res = await req.json()
+        console.log(res)
+        setCompanyDealsArray(res)
+    }
+
+    useEffect(() => {
+        fetchCompanyDeals()
+    },[])
+
+    let c = 0
+
+    let [companyContactsArray, setCompanyContactsArray] = useState([])
+
+    const fetchCompanyContacts = async () => {
+        const req = await fetch(`http://localhost:3000/company/${id}/contacts`)
+        const res = await req.json()
+        console.log(res)
+        setCompanyContactsArray(res)
+    }
+
+    useEffect(() => {
+        fetchCompanyContacts()
+    },[])
+
+
+
+
+
+
+
+
+
+
+
+
+    let [dealShow, setDealShow] = useState(false)
+
+    let dealSwitch = () => {
+        setDealShow(!dealShow)
+        console.log(dealShow)
+    }
+
+    let [contactShow, setContactShow] = useState(false)
+
+    let contactSwitch = () => {
+        setContactShow(!contactShow)
+    }
+
+
+    let [editState, setEditState] = useState(false)
+    let editClick = () => {
+        setEditState(!editState)
+        console.log(editState)
+    }
+
+
+
     return(
         <div className="details-card">
-            <h5>{company.name}</h5>
+            <h2>{company.name}</h2>
             <p>Company Id: {company.id}</p>
             <p>Company Address: {company.address}</p>
             <p>Country: {company.country}</p>
