57cb41c7ddd7319f948643c5a2519233625f1f2a
Aaron Fowlkes <afowlkes21@gmail.com>
2022-09-15 11:35:48 -0400

    has many deal creation is working

--- client/src/Components/Deal/DealsPage.js
+++ client/src/Components/Deal/DealsPage.js
@@ -23,40 +23,40 @@ function DealsPage() {
         return result
     }
 
-    const numDisplayer = (number) => {
-        if (Math.floor(number) != number) {
-            let x = number.toString()
-            let numArr = x.split('')
-            console.log(numArr)
-            let len = numArr.length
-            let a = numArr.indexOf('.')
-            for (let pos = a - 1; pos > 0; pos--) {
-                if ((a - pos) % 3 == 0 && len - pos != 0) {
-                    numArr.splice(pos, 0, ',')
-                }
-            }
-            let numWithCommas = ""
-            for (let i = 0; i < numArr.length; i++) {
-                numWithCommas += numArr[i]
-            }
-            return numWithCommas
-        } else if (Math.floor(number) == number) {
-            let x = number.toString()
-            let numArr = x.split('')
-            let len = numArr.length
-            for (let pos = numArr.length; pos > 0; pos--) {
-                if ((len - pos) % 3 == 0 && len - pos != 0) {
-                    numArr.splice(pos, 0, ',')
-                }
-            }
-            let numWithCommas = ""
-            for (let i = 0; i < numArr.length; i++) {
-                numWithCommas += numArr[i]
-            }
-            numWithCommas = numWithCommas + '.00'
-            return numWithCommas
-        }
-    }
+    // const numDisplayer = (number) => {
+    //     if (Math.floor(number) != number) {
+    //         let x = number.toString()
+    //         let numArr = x.split('')
+    //         console.log(numArr)
+    //         let len = numArr.length
+    //         let a = numArr.indexOf('.')
+    //         for (let pos = a - 1; pos > 0; pos--) {
+    //             if ((a - pos) % 3 == 0 && len - pos != 0) {
+    //                 numArr.splice(pos, 0, ',')
+    //             }
+    //         }
+    //         let numWithCommas = ""
+    //         for (let i = 0; i < numArr.length; i++) {
+    //             numWithCommas += numArr[i]
+    //         }
+    //         return numWithCommas
+    //     } else if (Math.floor(number) == number) {
+    //         let x = number.toString()
+    //         let numArr = x.split('')
+    //         let len = numArr.length
+    //         for (let pos = numArr.length; pos > 0; pos--) {
+    //             if ((len - pos) % 3 == 0 && len - pos != 0) {
+    //                 numArr.splice(pos, 0, ',')
+    //             }
+    //         }
+    //         let numWithCommas = ""
+    //         for (let i = 0; i < numArr.length; i++) {
+    //             numWithCommas += numArr[i]
+    //         }
+    //         numWithCommas = numWithCommas + '.00'
+    //         return numWithCommas
+    //     }
+    // }
 
 
     let navigate = useNavigate()
@@ -213,7 +213,7 @@ function DealsPage() {
                                     {/* <td onClick={() => handleContactClick(contact.id)}>{contact.name}</td> */}
                                     <td onClick={() => handleDealClick(deal.id)}>{deal.name}</td>
                                     <td>{deal.product}</td>
-                                    <td>{`$${numDisplayer(deal.value)}`}</td>
+                                    <td>{`$${(deal.value)}`}</td>
                                     <td>{deal.stage}</td>
                                     <td>{deal.active.toString()}</td>
                                     <td>{deal.status.toString()}</td>
